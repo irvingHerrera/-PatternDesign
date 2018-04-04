e5673eeda43e5b833a60283ddd202685d85cc102
Irving Herrera <irving.herreramolina@gmail.com>
2018-04-04 14:18:58 -0500

    Correccion

--- Pattern/FactoryMethod/Program.cs
+++ Pattern/FactoryMethod/Program.cs
@@ -6,9 +6,7 @@ namespace FactoryMethod
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Factory Method");
-            //Se crea una clase que se encarga de crear las instancias
-            // y un metodo que tiene la responsabilidad de crear las instancias
+            Console.WriteLine("Forma 1");
             MotorFactory motorFactoryMethod = new MotorFactory();
             var diselMethod = motorFactoryMethod.CreateInstance(TypeMotor.Disel);
             Console.WriteLine(diselMethod.InyectarCombustible(20));
@@ -16,21 +14,13 @@ namespace FactoryMethod
             Console.WriteLine(gasolinaMethod.InyectarCombustible(10));
             Console.ReadKey();
 
-            Console.WriteLine("Abstract Method");
-            //Esta parte se crean clases concretas definiendo una clase 
-            //factoria para la creacion de una clase concreta
-            //Se define el tipo de factoria
+            Console.WriteLine("Forma 2");
             IMotorFactory motorFactory = ObtenerMotor(TypeMotor.Gasolina);
 
-            //Se obtiene la instancia concreta
             IMotor gasolina = motorFactory.CreateInstance();
-            //Resultado
             Console.WriteLine(gasolina.InyectarCombustible(50));
 
-            //Se define el tipo de factoria y obtiene la instancia concreta
             IMotor disel = ObtenerMotor(TypeMotor.Disel).CreateInstance();
-
-            //Resultado
             Console.WriteLine(disel.InyectarCombustible(540));
 
             Console.ReadKey();
