c9d94e4d944238f0b3d35a2c70c343f127044894
Irving Herrera <irving.herreramolina@gmail.com>
2018-04-05 10:27:09 -0500

    Singleton

---
+++ Pattern/PatternSingleton/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PatternSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Patron singleton");
+            Console.WriteLine(Singleton.Instance.Name);
+            Console.WriteLine(Singleton.Instance.Date);
+            Console.WriteLine("Pausa...");
+            Thread.Sleep(100000);
+            Console.WriteLine(Singleton.Instance.Name);
+            Console.WriteLine(Singleton.Instance.Date);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Pattern/PatternSingleton/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PatternSingleton
+{
+    public class Singleton
+    {
+        private static readonly Lazy<Singleton> _instance = 
+                                new Lazy<Singleton>(() => new Singleton());
+
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+
+        private Singleton()
+        {
+            Name = "Pattern Singleto";
+            Date = DateTime.Now;
+        }
+
+        public static Singleton Instance
+        {
+            get => _instance.Value;
+        }
+    }
+}
